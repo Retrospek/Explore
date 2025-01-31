54a6d0056b7ba63ffcd37f6617e3b5065fb634a8
Arjun M <lordwhitley@gmail.com>
2025-01-31 10:25:07 -0600

    Finished Dense and Neural Network Class

--- NeuralNets/LinearlyConnected/functions.py
+++ NeuralNets/LinearlyConnected/functions.py
@@ -14,3 +14,29 @@ def xavier_normal(shape, n_in, n_out):
     """
     std = np.sqrt(2 / (n_in + n_out))  # Xavier standard deviation
     return np.random.normal(0, std, size=shape)
+
+def ReLU(x):
+    return np.maximum(0, x)
+
+def softmax(x):
+    exp_x = np.exp(x - np.max(x))  # Subtracting max for stability
+    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+
+class cross_entropy_loss:
+    def cost(y_true, y_pred):
+        """
+        Arguments:
+        - y_true: Actual classification, or regression values we're predicting
+        - y_pred: Predicted classification
+
+        Returns:
+        - Cost of a specific training example
+        """
+        y_pred = np.clip(y_pred, 1e-12, 1.0)  # Avoid log(0) instability
+        return -np.sum(y_true * np.log(y_pred)) / y_true.shape[0]
+    
+    def gradient(y_true, y_pred):
+        return y_pred - y_true
+
+
+ 
--- NeuralNets/LinearlyConnected/network.py
+++ NeuralNets/LinearlyConnected/network.py
@@ -1,5 +1,6 @@
 import numpy as np
-from NeuralNets.LinearlyConnected.functions import xavier_normal
+from functions import xavier_normal, ReLU, softmax
+
 """
 PLAN:
 - Develop a Dense Layer Class
@@ -8,13 +9,6 @@ PLAN:
 
 class Dense:
     def __init__(self, inputDim, outputDim):
-
-        # Obvious Shape initializations
-        self.inputDim = inputDim
-        self.outputDim = outputDim
-
-        # Not so obvious and intuitive weight initializations
-        # Math: Wx + b = Boom output (Z) => W must have some m x r dims and x must have r x n, where m can be = to n
         """
         EXAMPLE: x = [[1,2,3,4,5], [6,7,8,9,10]] of dim = (2(BS), 1(rows), 5(column)) and batch_size = 2.
                  But REALLY the dim = (batch_size, input) because the singleton dimension doesn't affect anything
@@ -22,21 +16,58 @@ class Dense:
                  as dot product will be far easier, and since it's conserved nothing changes
                  W must then have a dimension = (input, output)
         """
+
+        # Obvious Shape initializations
+        self.inputDim = inputDim
+        self.outputDim = outputDim
+
+        # Not so obvious and intuitive weight initializations
+        # Math: Wx + b = Boom output (Z) => W must have some m x r dims and x must have r x n, where m can be = to n
+    
         self.weights = xavier_normal((inputDim, outputDim), n_in=inputDim, n_out=outputDim) # (input,output) dim vector for weights
         self.biases = np.zeros((outputDim,)) # Just Zeroed out column vector
 
     def move(self, input):
         """
         Arguments:
+        - input of dim (batch_size, input_size)
+        - self of the node, which will hold the weights and biases
         """
 
-    
+        Zed = np.dot(input, self.weights) + self.biases
+
+        return Zed
+
 
 class basicNet:
-    def __init__(self, ):
+    def __init__(self, inputShape, outputShape):
         """
         Arguments:
         - Layer Sizes
         - Output Shape
         - Input Shape
         """
+
+        self.dense1 = Dense(inputShape, 32)
+        self.dense2 = Dense(32, 32)
+        self.dense3 = Dense(32, outputShape)
+
+
+        # For backpropogatoin we need to find a way to store the layers into one datastructure, so we can access and do back passing
+        self.layers = [
+            self.dense1,
+            self.dense2,
+            self.dense3
+        ]
+
+    def forward(self, x):
+        out = self.dense1.move(x)
+        out = ReLU(out)
+
+        out = self.dense2.move(out)
+        out = ReLU(out)
+        
+        out = self.dense3.move(out)
+        out = softmax(out)
+
+        return out
