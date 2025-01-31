41f40096bdd0bdfe90021286d30cb446f0a70557
Arjun M <lordwhitley@gmail.com>
2025-01-30 23:48:19 -0600

    Now I'm seriously done with the mock_data file

--- NeuralNets/LinearlyConnected/mock_data.py
+++ NeuralNets/LinearlyConnected/mock_data.py
@@ -90,3 +90,5 @@ batched_data = dataloader.data
 print(f"First DataLoader Dimension: {len(batched_data)}")
 print(f"Second DataLoader Dimension: {len(batched_data[0])}")
 print(f"Third DataLoader Dimension: {len(batched_data[0][0])}")
+print(f"X DataLoader Dimension: {len(batched_data[0][0][0])}")
+print(f"Y DataLoader Dimension: {batched_data[0][1][0]}")
