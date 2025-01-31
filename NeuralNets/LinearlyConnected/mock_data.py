42b646cea38935bd02e42c8939150e317f8b2282
Arjun M <lordwhitley@gmail.com>
2025-01-30 23:45:32 -0600

    Done with the data loaders

--- NeuralNets/LinearlyConnected/mock_data.py
+++ NeuralNets/LinearlyConnected/mock_data.py
@@ -10,8 +10,8 @@ def getIRIS():
     """
     iris = datasets.load_iris()
     irisDF = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-    print(irisDF.columns)
     irisDF['target'] = iris.target  
+
     return irisDF    
 
 class Dataset:
@@ -25,21 +25,20 @@ class Dataset:
         Goal: 
         - Initialization: Creates validIdx variables for later calling with the len method
         """
-
         self.data = data
         if shuffle:
             self.data = data.sample(frac=1)
         
+        self.batch_size = batch_size
+        self.target = self.data['target'].values
+        self.features = self.data.drop(columns=['target']).values
         self.data = self.data.values    
-        
-        self.target = self.data['target']
-        self.features = self.data.drop(columns=['target'])
 
         valid_indicies = []
 
-        for i in range(0, len(self.data), batch_size):
+        for i in range(0, len(data), batch_size):
             valid_indicies.append(i)
-        self.validIdx = valid_indicies
+        self.validIdx = np.array(valid_indicies)
         
     def len(self):
         """
@@ -53,15 +52,41 @@ class Dataset:
         Arguments:
         - idx: for the datalaoder method to continuously call the Dataset class
         """
-        #if idx <= self.validIdx: 
+
+        if idx <= len(self.validIdx):
+            # x_batch, y_batch return statement in an array format
+            return [self.features[idx: idx+ self.batch_size], self.target[idx: idx + self.batch_size]]
                                 
+class DataLoader:
+    def __init__(self, dataset):
+        """
+        Arguments:
+        - Dataset Object
+        """
+        self.dataset = dataset
+        self.valid_indices = dataset.validIdx
+
+        self.data = []
+        # Now we're going to initialize a data array for the dataloader
+        for idx in self.valid_indices:
+            if self.dataset.step(idx) != None:
+                self.data.append(self.dataset.step(idx)) 
+        print(f"Tester 1: {self.data}")
+        print(f"Tester 2: {self.data[0]}")
+        print(f"Tester 3: {self.data[0][0]}")
+
+        # Now you have stored the data in correct batch pairs
+        
+    def len(self):
+        return len(self.valid_indices)
+
 
 data = getIRIS()
-dataset = Dataset(data, test_size=0.2, shuffle=True, batch_size=32)
+dataset = Dataset(data, shuffle=True, batch_size=32)
 
+dataloader = DataLoader(dataset)
+batched_data = dataloader.data
 
-def DataLoader():
-
-
-
-    
+print(f"First DataLoader Dimension: {len(batched_data)}")
+print(f"Second DataLoader Dimension: {len(batched_data[0])}")
+print(f"Third DataLoader Dimension: {len(batched_data[0][0])}")
